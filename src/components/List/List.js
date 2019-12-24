74a1052cc7c61c6e4725cdd1d1e458f660b0f59e
Anna Kuszal <anna.kuszal@o2.pl>
2019-12-24 01:44:45 +0100

    Parse HTML code

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
@@ -27,7 +28,7 @@ class List extends React.Component {
         />
 
         <div className={styles.description}>
-          {this.props.description}
+          {ReactHtmlParser(this.props.description)}
         </div>
 
         <div className={styles.columns}>
