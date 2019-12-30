e1455150cd543b3925d0d3714bca19999d09850c
Anna Kuszal <anna.kuszal@o2.pl>
2019-12-30 19:29:07 +0100

    Turn off 'Columns' in component 'List'

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,15 +2,13 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+//import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
+  
 
   static propTypes = {
     title: PropTypes.node.isRequired,
@@ -23,35 +21,23 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   }
 
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }  
+  
 
   render() {
-    
+    const {title, image, description} = this.props;
+
     return (
       <section className={styles.component}>
         <Hero 
-          titleText={this.props.title}
-          imageSrc={this.props.image}  
+          titleText={title}
+          imageSrc={image}  
         />
 
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}
+          {ReactHtmlParser(description)}
         </div>
 
+        {/*
         <div className={styles.columns}>
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
@@ -60,8 +46,9 @@ class List extends React.Component {
 
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-          
         </div>
+          */}
+
       </section>
     );
   }
