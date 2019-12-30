371bcfa98c11f6b1d1083cbe638a7f51c48bc0b0
Anna Kuszal <anna.kuszal@o2.pl>
2019-12-30 23:46:06 +0100

    Render cards in 'Columns'

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 //import Creator from '../Creator/Creator.js';
-//import Card from '../Card/Card.js';
+import Card from '../Card/Card.js';
 //import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
@@ -16,15 +16,15 @@ class Column extends React.Component {
 
 
   render() {
-    const {title, icon} = this.props;
+    const {title, icon, cards} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}><span className={styles.icon}><Icon name={icon}/></span>{title}</h3>
-        {/*  
-        {this.state.cards.map(({key, ...cardProps}) => (
-          <Card key={key} {...cardProps} />
+         
+        {cards.map(cardData => (
+          <Card key={cardData.id} {...cardData} />
         ))}
-        
+        {/* 
         <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
         */}
       </section>
