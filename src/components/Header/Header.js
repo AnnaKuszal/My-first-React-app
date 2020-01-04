ce1ee68a528f034750b299ee0462f425ba8203a4
Anna Kuszal <anna.kuszal@o2.pl>
2020-01-04 15:01:15 +0100

    Update components - import from dataStore.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,21 +3,33 @@ import {NavLink, Link} from 'react-router-dom';
 import styles from './Header.scss';
 import Container from '../Container/Container.js';
 import Icon from '../Icon/Icon.js';
+import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
 
 class Header extends React.Component {
 
+  static propTypes = {
+    icon: PropTypes.node.isRequired,
+  }
+
+  static defaultProps = {
+    icon: settings.navbar.icon,
+  }
+
 
   render () {
+    const {icon} = this.props;
     return (
       <header className={styles.component}>
         <Container>
           <div className={styles.wrapper}>
             <Link className={styles.logo} to='/'>
-              <Icon name='cat' />
+              <Icon name={icon} />
             </Link>
             <nav>
-              <NavLink exact to='/' activeClassName='active'>Home</NavLink>
-              <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
+              <NavLink exact to='/' activeClassName='active'>{settings.navbar.links.home}</NavLink>
+              <NavLink exact to='/info' activeClassName='active'>{settings.navbar.links.info}</NavLink>
+              <NavLink exact to='/FAQ' activeClassName='active'>{settings.navbar.links.faq}</NavLink>
             </nav>
           </div>
         </Container>
